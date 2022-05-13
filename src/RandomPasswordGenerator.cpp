6e261497ef3654aadf19b86c839b872c84f6aceb
brenoreilly17 <brenoreilly17@gmail.com>
2022-05-12 22:18:03 -0400

    Experimenting with different randomization techniques.

--- src/RandomPasswordGenerator.cpp
+++ src/RandomPasswordGenerator.cpp
@@ -29,26 +29,27 @@ void Password::choose_specs() {
 }
 
 void Password::random_add() {
-  uint32_t num = rand() % 10 + 1;
+  uint32_t i = 0;
   if (cap) {
-    srand(time(nullptr));
-    char character = num + 'A';
+    srand(i);
+    char character = rand();
     pass += character;
   }
   if (low || !cap) {
-    char character = num + 'a';
-    pass += character;
+//    char character = num + 'a';
+//    pass += character;
   }
   if (!low && !cap) {
-    uint32_t i = 0;
-    if (i % 2 == 0) {
-      char character = num + 'a';
-      pass += character;
-    } else {
-      char character = num + 'A';
-      pass += character;
-    }
+//    uint32_t i = 0;
+//    if (i % 2 == 0) {
+//      char character = num + 'a';
+//      pass += character;
+//    } else {
+//      char character = num + 'A';
+//      pass += character;
+//    }
   }
+  ++i;
 }
 
 std::string Password::password_generator() {
