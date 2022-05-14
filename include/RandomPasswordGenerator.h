c1d0d560109aa93ff89101c7ad6396cf3ae28c51
brenoreilly17 <brenoreilly17@gmail.com>
2022-05-14 13:14:02 -0400

    Commenting code

--- include/RandomPasswordGenerator.h
+++ include/RandomPasswordGenerator.h
@@ -9,11 +9,37 @@
 
 class Password {
 public:
+    /**
+     * @Brief Constructor for the password
+     */
   Password();
+
+  /**
+   * @Brief Choose specifications for your password
+   */
   void choose_specs();
+
+  /**
+   * @Brief Generates the password based on specs
+   * @return Password
+   */
   std::string password_generator();
+
+  /**
+   * @Brief Randomly generates the password
+   */
   void random_add();
+
+  /**
+   * @Brief Determines if user input is valid (for lowercase spec)
+   * @return True or false
+   */
   bool lowercaseValidInput() const;
+
+  /** @Brief
+   * Determines if user input is valid (for uppercase spec)
+   * @return True or false
+   */
   bool uppercaseValidInput() const;
   bool number_choice();
   std::string input_numbers();
