c3256203caa49cbaa10d2feb30f1193340fac837
brenoreilly17 <brenoreilly17@gmail.com>
2022-05-22 23:26:22 -0400

    Comments

--- src/RandomPasswordGenerator.cpp
+++ src/RandomPasswordGenerator.cpp
@@ -161,7 +161,7 @@ int main() {
   if (new_pass.number_choice()) {
     new_pass.password_generator();
     new_pass.input_numbers();
-    // Output password to console
+    // Output password  console
     std::cout << new_pass.pass;
   }
 
