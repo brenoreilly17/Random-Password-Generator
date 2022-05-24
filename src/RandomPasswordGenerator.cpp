6138bd7d6b804a19596f55dc40170b1d8708b9ee
brenoreilly17 <brenoreilly17@gmail.com>
2022-05-23 21:48:32 -0400

    Comments

--- src/RandomPasswordGenerator.cpp
+++ src/RandomPasswordGenerator.cpp
@@ -167,7 +167,7 @@ int main() {
 
   // If the user did not want numbers in password
   else {
-      // Output password to
+      // Output password to console
     std::cout << new_pass.password_generator() << std::endl;
   }
 
