295ff051f049d7e50e1f1dc2b9c2561088e860a5
brenoreilly17 <brenoreilly17@gmail.com>
2022-06-08 21:41:23 -0400

    Comments

--- src/RandomPasswordGenerator.cpp
+++ src/RandomPasswordGenerator.cpp
@@ -66,7 +66,7 @@ void Password::choose_specs() {
 
 bool Password::number_choice() {
   std::cout << "Would you like numbers added to your password?" << std::endl;
-  // Number input
+
   std::cin >> numbers;
   if (numbers == 'Y' || numbers == 'y') {
     return true;
